cb007a134b60a7b9fb0e2ac8a0db0d103b6f37bd
Naman-bangari <143520637+Naman-bangari@users.noreply.github.com>
2024-03-13 23:15:28 +0530

    2

--- Validator/home/urls.py
+++ Validator/home/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 urlpatterns = [
-    path("", views.index,name='home'),
+    path("",views.home1,name='home1'),
+    path("index", views.index,name='index'),
     path("about",views.about,name='about'),
     path("services",views.services,name='services'),
-      path("contact",views.contact,name='contact'),
+    path("contact",views.contact,name='contact'),
+    path("contact",views.contact,name='contact'),
+    
 ]
--- Validator/home/views.py
+++ Validator/home/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render ,HttpResponse,redirect
 # Create your views here.
 def index(request):
     code1 = request.session.get('code1', None)  # Retrieve code1 from session or None if not found
-    if request.method == 'GET':
-        new_code1 = request.GET.get('code')
+
+    if request.method == 'POST':
+        new_code1 = request.POST.get('code')
         if new_code1 is not None:  # Check if new_code1 is not None
             request.session['code1'] = new_code1  # Store new_code1 in session
             code1 = new_code1
-        #print("code1:", code1)
+
     return render(request, 'index.html', {'code1': code1})
+
 def about(request):
     code1 = request.session.get('code1')  # Retrieve code1 from session
     return render(request, 'about.html', {'code1': code1})
@@ -19,4 +21,6 @@ def services(request):
  
 def contact(request):
      return render(request,'contact.html')
+def home1(request):
+     return render(request,'home1.html')
   
